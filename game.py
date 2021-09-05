af721ae6df39890083328e1991cacb3371be1643
Alan <alannava@hotmail.com>
2021-09-05 15:50:30 -0500

    Parte 2, se agrega el movimiento a los personajes

--- game.py
+++ game.py
@@ -19,6 +19,11 @@ background = pygame.image.load("Fondos/fondo1.png")
 
 #CARACTERISTICAS DEL PERSONAJE PRINCIPAL
 playerImg = pygame.image.load('Imagenes/doctor1.png')
+playerRect = playerImg.get_rect()
+playerRect.x, playerRect.y = 300, 500
+player_movementX = 0
+salto = 10
+Jumping = False
 
 #FUNCIÓN QUE COLOCA AL PERSONAJE PRINCIPAL EN PANTALLA CON SUS MOVIMIENTOS
 def fijar_personaje_principal(personajeX, personajeY):
@@ -41,12 +46,18 @@ def fijar_plataformas():
 virusImg = []
 virusRectX = []
 virusRectY = []
+virusRect = []
+virus_movementX = []
+virus_movementY = []
 num_of_enemies = 3
 
 for i in range(num_of_enemies):
     virusImg.append(pygame.image.load('Imagenes/virus2.png'))
     virusRectX.append(random.randint(100,900))
     virusRectY.append(random.randint(50,450))
+    virusRect.append(virusImg[i].get_rect())
+    virus_movementX.append( random.choice([4,-4]) )
+    virus_movementY.append(  random.choice([4,-4]) )
 
 #FUNCIÓN QUE COLOCA AL PERSONAJE ENEMIGO EN PANTALLA CON SUS MOVIMIENTOS
 def fijar_personaje_enemigo():
@@ -58,12 +69,16 @@ def fijar_personaje_enemigo():
 personasImg = []
 personasRectX = []
 personasRectY = []
+personasRect = []
+personas_movementX = []
 num_of_personas = 5
 
 for i in range(num_of_personas):
     personasImg.append(pygame.image.load('Imagenes/persona1.png'))
     personasRectX.append(random.randint(100,900))
     personasRectY.append(600)
+    personasRect.append(personasImg[i].get_rect())
+    personas_movementX.append( random.choice([4,-4]) )
 
 #FUNCION QUE FIJA A LOS PERSONAJES EXTRAS EN PANTALLA
 def fijar_personas_extras():
@@ -106,12 +121,65 @@ while jugando:
         if event.type == pygame.QUIT:
             jugando = False
 
+        #MOVIMIENTOS DE LOS PERSONAJES CON LAS TECLAS (WASD)
+            #ESTO PASA CUANDO PRECIONAMOS UNA TECLA
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                player_movementX = -5
+            if event.key == pygame.K_d:
+                player_movementX = 5
+            if event.key == pygame.K_w:
+                Jumping = True
+            #ESTO PASA CUANDO SOLTAMOS UNA TECLA
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a or event.key == pygame.K_d:
+                player_movementX = 0
+
+
+    #ASIGNANDO RESTRICCIONES A PERSONAJES POR LA RESOLUCION
+        #PERSONAJE PRINCIPAL
+    playerRect.x += player_movementX
+    if playerRect.x<0:
+        playerRect.x=0
+    if playerRect.x >1230:
+        playerRect.x = 1230
+        #PERSONAJE ENEMIGO
+    for i in range(num_of_enemies):
+        if virusRectX[i] <=0:
+            virus_movementX[i] = 4
+        if virusRectX[i] >=1220:
+            virus_movementX[i] = -4
+        if virusRectY[i] <=0:
+            virus_movementY[i] = 4
+        if virusRectY[i] >=580:
+            virus_movementY[i] = -4
+        virusRectX[i] += virus_movementX[i]
+        virusRectY[i] += virus_movementY[i]
+        #PERSONAS EXTRAS
+    for i in range(num_of_personas):
+        if personasRectX[i]<0:
+            personas_movementX[i] = 4
+        if personasRectX[i] >1230:
+            personas_movementX[i] = -4
+        personasRectX[i]  += personas_movementX[i]
+
+
+
+    #SALTO DEL PERSONAJE PRINCIPAL
+    if Jumping:
+        if salto >= -10:
+            playerRect.y -= (salto * abs(salto)) * 0.5
+            salto -= 1
+        else:   
+            salto = 10
+            Jumping = False
+
     #EJECUTANDO ACTUALIZACIONES
-    fijar_personaje_principal(300, 500)
     fijar_plataformas()
     fijar_personaje_enemigo()
     fijar_personas_extras()
     fijar_corazones()
     fijar_puntaje()
+    fijar_personaje_principal(playerRect.x, playerRect.y)
     pygame.display.update()
     clock.tick(30)
